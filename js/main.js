1b0fcd8cb96eba4a053437825c7b548f8146c78d
milenad <milena.dampc@gmail.com>
2024-03-12 16:40:56 +0100

    skonczone

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 7;
+let secondNumber = 3;
+
+function dodawanie(num1, num2){
+    let suma = num1 + num2;
+    return suma;
+
+}
+
+let wynik = dodawanie(firstNumber, secondNumber);
+console.log(wynik);
